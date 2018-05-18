e28a8ad137a850d92c91c0afcbdeeb7cc2d4ee0f
Vultor <fred.vultor@gmail.com>
2018-05-18 16:27:39 -0300

    change isomers format to RSG format

---
+++ code/python/splitIsomers.py
@@ -0,0 +1,52 @@
+import glob
+
+
+def splitIsomerFile(fileName): 
+	if fileName.find('counting') > -1:
+		return
+	isomerFile = open(fileName,"r")
+	fileStream = isomerFile.read().splitlines()
+	isomerFile.close()
+	listR = []
+	listS = []
+	listG = []
+	i = 1
+	while i < len(fileStream):
+		if fileStream == '':
+			continue
+		if i < len(fileStream)-1:
+			if fileStream[i+1] == '':
+				listG.append(fileStream[i])
+				i+=2
+			else:
+				listR.append(fileStream[i])
+				listS.append(fileStream[i+1])
+				i+=3
+		else:
+			listG.append(fileStream[i])
+			i+=1
+
+	isomerFile = open(fileName, "w")
+	isomerFile.write("{}\n".format(fileStream[0]))
+	isomerFile.write("G\n")
+	for isomer in listG:
+		isomerFile.write("{}\n".format(isomer))
+	isomerFile.write("R\n")
+	for isomer in listR:
+		isomerFile.write("{}\n".format(isomer))
+	isomerFile.write("S\n")
+	for isomer in listS:
+		isomerFile.write("{}\n".format(isomer))
+	isomerFile.close()
+	return
+
+
+stereoList = glob.glob('StereoisomerList//*')
+for cn in stereoList:
+	allGeo = glob.glob(cn + '//*')
+	for geo in allGeo:
+		allIsoFiles = glob.glob(geo + '//*')
+		for isoFile in allIsoFiles:
+			splitIsomerFile(isoFile)
+			
+	
