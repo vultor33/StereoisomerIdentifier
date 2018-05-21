3f8bf2d393fb5ec22ca0e5b742c8ea1ee4707567
Vultor <fred.vultor@gmail.com>
2018-05-21 16:04:35 -0300

    chelates G working

--- code/python/Enumeration.py
+++ code/python/Enumeration.py
@@ -25,7 +25,7 @@ class Enumeration:
 			self.__allRot.append(rotI)
 			
 	def readScaffold(self, geoCode):
-		fileName = 'Stereoisomerlist//CN6//OC-6//OC-6-Mabcdef.csv'  # LEEEEEEEEEEEEEEEEEEEEERRRR A PARTIR DO GEO CODE
+		fileName = 'Stereoisomerlist//CN4//SP-4//SP-4-Mabcd.csv'  # LEEEEEEEEEEEEEEEEEEEEERRRR A PARTIR DO GEO CODE
 		scaInput = open(fileName,"r")
 		scaFileStream = scaInput.read().splitlines()
 		i = 2
@@ -55,32 +55,37 @@ class Enumeration:
 		self._correctSubstratal(substratalG, 'G')
 		self._correctSubstratal(substratalR, 'R')
 		self._correctSubstratal(substratalS, 'S')
-		print('sca G: ',self.__substratalG)
-		print('sca R: ',self.__substratalR)
-		print('sca S: ',self.__substratalS)
 	
 
 	def makeEnumeration(self, colorFirst, chelateFirst, rcw, stereoRemoved): # REFAZEEER
 		i = 0
-		while i < len(self.__substratal) - 1:
+		while i < len(self.__substratalG) - 1:
 			j = i + 1
 			if i in stereoRemoved:
 				i += 1
 				continue
-			while j < len(self.__substratal):
+			while j < len(self.__substratalG):
 				if j in stereoRemoved:
 					j += 1
 					continue
-				if self._compareWithRotation(
-					self._painting(colorFirst,self.__substratal[i]),
-					self._chelateRepositon(chelateFirst,self.__substratal[i]),
-					self._painting(colorFirst,self.__substratal[j]),
-					self._chelateRepositon(chelateFirst,self.__substratal[j])):
+				#print(i, ' - ', j)
+				if self._compareWithRotation(#inside compare:
+				self._painting(colorFirst,self.__substratalG[i]),
+				self._chelateRepositon(chelateFirst,self.__substratalG[i]),
+				self._painting(colorFirst,self.__substratalG[j]),
+				self._chelateRepositon(chelateFirst,self.__substratalG[j])):
+					#print('removed: ', j)
 					rcw.append(i)
 					stereoRemoved.append(j)
-					print(i, ' equal ', j)
 				j+=1
 			i+=1
+		
+
+	def printInfo(self):
+		print('sca G: ',self.__substratalG)
+		print('sca R: ',self.__substratalR)
+		print('sca S: ',self.__substratalS)
+
 
 	#Rotating permutations
 	def _applyRotationPermut(self, lisPermut, rotation):
@@ -113,10 +118,9 @@ class Enumeration:
 
 	#COMPARING
 	def _compareWithRotation(self, lisPermut1, lisChelate1, lisPermut2, lisChelate2):
-		for chel in lisChelate1:
-			chel.sort()
-		for chel in lisChelate2:
-			chel.sort()
+		for iChel in range(len(lisChelate1)):
+			lisChelate1[iChel].sort()
+			lisChelate2[iChel].sort()
 		
 		if lisPermut1 == lisPermut2:
 			if lisChelate1 == lisChelate2:
@@ -127,13 +131,15 @@ class Enumeration:
 			if permutRotI == lisPermut1:
 				equal = True
 				i = 0
-				while i < len(lisChelate2):
-					chelRotI = self._applyRotationChelate(lisChelate2[i],rotation)
-					chelRotI.sort()
-					if chelRotI in lisChelate1:
+				chelRotI = []
+				for i in range(len(lisChelate2)):
+					rotI = self._applyRotationChelate(lisChelate2[i],rotation)
+					rotI.sort()
+					chelRotI.append(rotI)
+				for i in range(len(chelRotI)):
+					if not chelRotI[i] in lisChelate1:
 						equal = False
 						break
-					i+=1
 				if equal:
 					return equal
 	
--- code/python/mainEnumerate.py
+++ code/python/mainEnumerate.py
@@ -8,46 +8,35 @@ import csv
 import json
 from DataAllFormulas import allFormList4
 
-for form in allFormList8:
-	print(form)
+#for form in allFormList8:
+#	print(form)
 
-exit()
-
-with open('allFormulas.csv', 'r') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-	for row in spamreader:
-		#print(row)
-		saveList = []
-		for elem in row[1]:
-			if elem.isdigit():
-				saveList.append(int(float(elem)))
-		#print(saveList)
-		lista2 = json.load(row[2])
-		print(lista2)
-		#print('lista:  ',row[1])
-		#print (', '.join(row))
-
-exit()
-
-
-exit()
-
-substratalScaffoldTemp = [[1,2,3,4],[1,3,2,4],[1,2,4,3]]
-allRotations = [[1,2,3,0],[2,3,0,1],[3,0,1,2],[0,3,2,1],[2,1,0,3],[1,0,3,2],[3,2,1,0]]
-colorFirst = [1,1,2,2]
-chelateFirst = []
-stereoRemoved = []
-rcw = []
+#substratalScaffoldTemp = [[1,2,3,4],[1,3,2,4],[1,2,4,3]]
+#allRotations = [[1,2,3,0],[2,3,0,1],[3,0,1,2],[0,3,2,1],[2,1,0,3],[1,0,3,2],[3,2,1,0]]
+#colorFirst = [1,1,2,2]
+#chelateFirst = []
 
 obj1 = Enumeration()
 obj1.readRotations(41)
 obj1.readScaffold(41)
-exit()
-obj1.setSubstratal(substratalScaffoldTemp)
-del allRotations
-del substratalScaffoldTemp
+#exit()
+#obj1.setSubstratal(substratalScaffoldTemp)
+#del allRotations
+#del substratalScaffoldTemp
 
-obj1.makeEnumeration(colorFirst, chelateFirst, rcw, stereoRemoved)
+for i in range(len(allFormList4)):
+#	if i != 8:
+#		continue
+	stereoRemoved = []
+	rcw = []
+	if allFormList4[i][2] == ['None']:
+		obj1.makeEnumeration(allFormList4[i][1], [], rcw, stereoRemoved)
+	else:
+		obj1.makeEnumeration(allFormList4[i][1], allFormList4[i][2], rcw, stereoRemoved)
+	
+	print(allFormList4[i][0],'  Total:  ',3 - len(stereoRemoved))
+#	exit()
 
-print('rcw:  ', rcw)
-print('removed: ', stereoRemoved)
+	
+#print('rcw:  ', rcw)
+#print('removed: ', stereoRemoved)
